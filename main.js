fc661b922820c95891f63993d71e75a582fad2dc
ShenghaoisYummy <hsupisces@hotmail.com>
2024-02-12 14:23:10 +1100

    added animation switch

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ import Cube from "./src/Model/cube";
 import CircularSector from "./src/Model/circularSector";
 
 const app = new Three_App();
-const cube = new Cube();
+const cube = new Cube({ animateSwitch: true });
+
 const circular_sector = new CircularSector();
 
 app.addModel(cube);
+app.addModel(circular_sector);
--- src/Model/cube.js
+++ src/Model/cube.js
@@ -2,26 +2,38 @@ import * as THREE from "three";
 
 class Cube {
   geometry = new THREE.BoxGeometry(1, 1, 1);
+
   material = new THREE.MeshBasicMaterial({
     wireframe: true,
     wireframeLinecap: "round",
     color: 0x00ff00,
   });
 
-  constructor(geometry, material) {
-    this.init(geometry, material);
+  animateSwitch = true;
+  guiInteractiveSwitch = true;
+
+  constructor(props) {
+    this.geometry = props?.geometry || this.geometry;
+    this.material = props?.material || this.material;
+    if (props?.animateSwitch != undefined) {
+      this.animateSwitch = !!props.animateSwitch;
+    }
+    if (props?.guiInteractiveSwitch != undefined) {
+      this.guiInteractiveSwitch = !!props.guiInteractiveSwitch;
+    }
+    this.init();
   }
 
-  init = (geometry, material) => {
-    if (geometry) this.geometry = geometry;
-    if (material) this.material = material;
+  init = () => {
     this.mesh = new THREE.Mesh(this.geometry, this.material);
   };
 
   animate = () => {
-    this.mesh.rotation.x += 0.01;
-    this.mesh.rotation.y += 0.01;
-    this.mesh.rotation.z += 0.01;
+    if (this.animateSwitch) {
+      this.mesh.rotation.x += 0.01;
+      this.mesh.rotation.y += 0.01;
+      this.mesh.rotation.z += 0.01;
+    }
   };
 }
 
